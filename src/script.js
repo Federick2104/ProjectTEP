a7e217d3cb76e4dbcdf368cad073641a19ab6d87
Federick2104 <federicoschianchi2004@gmail.com>
2022-03-01 09:53:51 +0100

    ResponsiveFixes

--- src/script.js
+++ src/script.js
@@ -1,85 +1,92 @@
 import './style.css'
 import * as THREE from 'three'
 import AOS from 'aos'
-import GLTFLoader from 'three-gltf-loader'
+// import GLTFLoader from 'three-gltf-loader'
 
-const loader = new GLTFLoader()
-loader.load(
-  'src/model/untitled.gltf',
-  (gltf) => {
-    // called when the resource is loaded
-    scene.add(gltf.scene)
-  },
-  (xhr) => {
-    // called while loading is progressing
-    console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`)
-  },
-  (error) => {
-    // called when loading has errors
-    console.error('An error happened', error)
-  }
-)
+const resizing = () => {
+  console.log('resizeeeeeeeee')
+  const w = window.innerWidth
+  const h = window.innerHeight
 
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x000000)
-const canvas1 = document.querySelector('.webgl')
-// const loader = new THREE.TextureLoader() , map: loader.load('src/texture/earthmap1k.jpg')
-// creazione sfera/cubo
-const geometry = new THREE.BoxGeometry(2, 2, 2)
-const material = new THREE.MeshLambertMaterial({ color: 0x12AAB8 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(-2, 0, 0)
-scene.add(mesh)
+  // const loader = new GLTFLoader()
+  // loader.load(
+  //   'src/model/untitled.gltf',
+  //   (gltf) => {
+  //   // called when the resource is loaded
+  //     scene.add(gltf.scene)
+  //   },
+  //   (xhr) => {
+  //   // called while loading is progressing
+  //     console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`)
+  //   },
+  //   (error) => {
+  //   // called when loading has errors
+  //     console.error('An error happened', error)
+  //   }
+  // )
 
-// 2 Cubo
-const geometry2 = new THREE.BoxGeometry(2, 2, 2)
-const material2 = new THREE.MeshLambertMaterial({ color: 0x0c9e0f })
-const mesh2 = new THREE.Mesh(geometry2, material2)
-mesh2.position.set(2, 0, 0)
-scene.add(mesh2)
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x000000)
+  const canvas1 = document.querySelector('.webgl')
+  // const loader = new THREE.TextureLoader() , map: loader.load('src/texture/earthmap1k.jpg')
+  // creazione sfera/cubo
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
+  const material = new THREE.MeshLambertMaterial({ color: 0x12AAB8 })
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.position.set(-2, 0, 0)
+  scene.add(mesh)
 
-// luce
-const ambientlight = new THREE.AmbientLight(0xffffff, 0.6)
-scene.add(ambientlight)
-// luce direzionale
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
-dirLight.position.set(10, 20, 0) // x, y, z
-scene.add(dirLight)
+  // 2 Cubo
+  const geometry2 = new THREE.BoxGeometry(2, 2, 2)
+  const material2 = new THREE.MeshLambertMaterial({ color: 0x0c9e0f })
+  const mesh2 = new THREE.Mesh(geometry2, material2)
+  mesh2.position.set(2, 0, 0)
+  scene.add(mesh2)
 
-// camera
-const width = 10
-const height = width * (window.innerHeight / window.innerWidth)
-const camera = new THREE.OrthographicCamera(
-  width / -2, // left
-  width / 2, // right
-  height / 2, // top
-  height / -2, // bottom
-  1, // near
-  100 // far
-)
+  // luce
+  const ambientlight = new THREE.AmbientLight(0xffffff, 0.6)
+  scene.add(ambientlight)
+  // luce direzionale
+  const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
+  dirLight.position.set(10, 20, 0) // x, y, z
+  scene.add(dirLight)
 
-camera.position.set(4, 4, 4)
-camera.lookAt(0, 0, 0)
+  // camera
+  const width = 10
+  const height = width * (h / w)
+  const camera = new THREE.OrthographicCamera(
+    width / -2, // left
+    width / 2, // right
+    height / 2, // top
+    height / -2, // bottom
+    1, // near
+    100 // far
+  )
 
-// Rendering profile
-const renderer = new THREE.WebGLRenderer(
-  { antialias: true, canvas: canvas1 })
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+  camera.position.set(4, 4, 4)
+  camera.lookAt(0, 0, 0)
 
-// Animazione della terra che gira
-const animate = () => {
-  window.requestAnimationFrame(animate)
-  mesh2.rotation.x -= 0.0040
-  mesh.rotation.y -= 0.0035
-  render()
-}
-
-// Renderinig della scena e camera
-const render = () => {
+  // Rendering profile
+  const renderer = new THREE.WebGLRenderer(
+    { antialias: true, canvas: canvas1 })
+  renderer.setSize(w, h)
   renderer.render(scene, camera)
+
+  // Animazione della terra che gira
+  const animate = () => {
+    window.requestAnimationFrame(animate)
+    mesh2.rotation.x -= 0.0040
+    mesh.rotation.y -= 0.0035
+    render()
+  }
+
+  // Renderinig della scena e camera
+  const render = () => {
+    renderer.render(scene, camera)
+  }
+  animate()
 }
-animate()
+
 AOS.init() // Animzioni on scroll realizzate da michalsnik at michalsnik.github.io
 
 // -------------------------------------------------------------------------------- //
@@ -128,6 +135,8 @@ const domandeQuiz = [{
 }]
 
 window.onload = () => {
+  resizing()
+  window.onresize = resizing
   const quiz = document.querySelector('.answ-card')
   const buttons = document.querySelectorAll('.quiz-button')
   buttons.forEach((b, i) => {
