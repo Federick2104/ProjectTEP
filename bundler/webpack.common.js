04106b04079ff65f7fed2e4f4e7c13b9e1b9d9b1
XiiK <marelestef@gmail.com>
2022-03-04 11:38:43 +0100

    webpack bugged

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,15 +1,15 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/script.js'),
   output:
-    {
-      filename: 'bundle.[contenthash].js',
-      path: path.resolve(__dirname, '../dist')
-    },
+  {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, '../dist')
+  },
   devtool: 'source-map',
   plugins:
     [
@@ -22,67 +22,73 @@ module.exports = {
         template: path.resolve(__dirname, '../src/index.html'),
         minify: true
       }),
-      new MiniCSSExtractPlugin()
+      new HtmlWebpackPlugin({
+        template: './src/game.html',
+        inject: true,
+        chunks: ['index'],
+        filename: 'game.html'
+      }),
+      new MiniCssExtractPlugin()
     ],
   module:
-    {
-      rules:
-        [
-          // HTML
-          {
-            test: /\.(html)$/,
-            use: ['html-loader']
-          },
+  {
+    rules:
+      [
+        // HTML
+        {
+          test: /\.(html)$/,
+          use: ['html-loader']
+        },
 
-          // JS
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use:
-                [
-                  'babel-loader'
-                ]
-          },
+        // JS
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use:
+            [
+              'babel-loader'
+            ]
+        },
 
-          // CSS
-          {
-            test: /\.css$/,
-            use:
-                [
-                  MiniCSSExtractPlugin.loader,
-                  'css-loader'
-                ]
-          },
+        // CSS
+        {
+          test: /\.css$/,
+          use:
+            [
+              MiniCssExtractPlugin.loader,
+              'css-loader'
+            ]
+        },
 
-          // Images
-          {
-            test: /\.(jpg|png|gif|svg)$/,
-            use:
-                [
-                  {
-                    loader: 'file-loader',
-                    options:
-                        {
-                          outputPath: 'assets/images/'
-                        }
-                  }
-                ]
-          },
+        // Images
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use:
+            [
+              {
+                loader: 'file-loader',
+                options:
+                {
+                  outputPath: 'assets/images/'
+                }
+              }
+            ]
+        },
 
-          // Fonts
-          {
-            test: /\.(ttf|eot|woff|woff2)$/,
-            use:
-                [
-                  {
-                    loader: 'file-loader',
-                    options:
-                        {
-                          outputPath: 'assets/fonts/'
-                        }
-                  }
-                ]
-          }
-        ]
-    }
+        // Fonts
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          use:
+            [
+              {
+                loader: 'file-loader',
+                options:
+                {
+                  outputPath: 'assets/fonts/'
+                }
+              }
+            ]
+        }
+      ]
+  }
 }
