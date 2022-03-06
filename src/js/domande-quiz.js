e9f100e3a9bc85d586acc0071369fcfe0f66b34a
Federick2104 <federicoschianchi2004@gmail.com>
2022-03-06 23:09:36 +0100

    Domande e quiz

--- src/js/domande-quiz.js
+++ src/js/domande-quiz.js
@@ -11,31 +11,31 @@ export const scope = {
 // c = risposta corretta
 // desc = descrizione finale
 export const domandeQuiz = [{
-  d1: '111Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp1: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d1: 'Quanti chili di plastica ci sono nel Mar Mediterraneo?',
+  risp1: ['3000', '4000', '5000', '60000'],
   c1: 2,
-  d2: 'Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp2: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d2: 'Una nave da Crociera quanti litri di rifiuti accumola in una settimana?',
+  risp2: ['800.000!', '500.000', '1.200.000', '300.000'],
   c2: 2,
-  desc: 'ajskldfhkashdfkasdhfjklh ashdfkjlhaskjdfhalkjshdf a kjdhfk ks akjdhf kjhaljkadklj hll'
+  desc: 'Greve zi; Il Mediterraneo è tra i mari più inquinati dalla plastica: si trovano microplastiche e macroplastiche dalla superficie ai fondali e lungo lintero perimetro costiero.'
 }, {
-  d1: '222Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp1: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d1: 'Quale oggetto è il più inquinante per lambiente?',
+  risp1: ['Chewing Gum', 'Pneumatico', 'Sigarette', 'Cotton Fioc'],
   c1: 2,
 
-  d2: 'Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp2: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d2: 'Dove si trova linceneritore più grande dEuropa?',
+  risp2: ['Monaco', 'Brescia', 'Stoccolma', 'Le Mans'],
   c2: 2,
-  desc: 'ajskldfhkashdfkasdhfjklh ashdfkjlhaskjdfhalkjshdf a kjdhfk ks akjdhf kjhaljkadklj hll'
+  desc: 'Greve zi'
 
 }, {
-  d1: '333Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp1: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d1: 'Quanti animali muoiono ogni anno per i rifiuti di plastica negli oceani?',
+  risp1: ['3mln', '1.5mln', '2.5mln', '500.000'],
   c1: 2,
 
-  d2: 'Animzioni on scroll realizzate da michalsnik at michalsnik.github.io??',
-  risp2: ['risposta 1', 'risposta 2', 'risposta3', 'risposta 4'],
+  d2: 'Quali tra questi animali marini è quello più a rischio di esitinzione?',
+  risp2: ['Balenottere Azzurre', 'Delfino Vaquita', 'Tartaruga Embricata', 'Tonno Rosso'],
   c2: 2,
-  desc: 'ajskldfhkashdfkasdhfjklh ashdfkjlhaskjdfhalkjshdf a kjdhfk ks akjdhf kjhaljkadklj hll'
+  desc: 'Greve zi'
 
 }]
