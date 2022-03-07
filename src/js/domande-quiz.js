a6f5a59d42adc90df73ac092657fbfcc8abf5ede
Federick2104 <federicoschianchi2004@gmail.com>
2022-03-07 20:25:27 +0100

    suca

--- src/js/domande-quiz.js
+++ src/js/domande-quiz.js
@@ -11,31 +11,31 @@ export const scope = {
 // c = risposta corretta
 // desc = descrizione finale
 export const domandeQuiz = [{
-  d1: 'Quanti chili di plastica ci sono nel Mar Mediterraneo?',
-  risp1: ['3000', '4000', '5000', '60000'],
-  c1: 2,
+  d1: 'Quante tonnellate di plastica vengono scaricate ogni giorno nel Mar Mediterraneo?',
+  risp1: ['341.000', '229.000', '564.000', '232.000'],
+  c1: 1,
   d2: 'Una nave da Crociera quanti litri di rifiuti accumola in una settimana?',
-  risp2: ['800.000!', '500.000', '1.200.000', '300.000'],
-  c2: 2,
-  desc: 'Greve zi; Il Mediterraneo è tra i mari più inquinati dalla plastica: si trovano microplastiche e macroplastiche dalla superficie ai fondali e lungo lintero perimetro costiero.'
+  risp2: ['800.000', '500.000', '1.200.000', '300.000'],
+  c2: 0,
+  desc: 'Si stima che un totale di circa 8 milioni di tonnellate di plastica finiscano nei nostri oceani ogni anno. La Ellen MacArthur Foundation e il World Economic Forum prevedono che entro il 2050 ci sarà, in peso, più plastica che pesce nell’oceano.<br><br>1) Vengono scaricati 229.000 chili di plastica ogni giorno, per questo il Mediterraneo è tra i mari più inquinati dalla plastica: dove si trovano microplastiche e macroplastiche dalla superficie ai fondali e lungo lintero perimetro costiero.<br><br>2) La risposta corretta è 800.000. Inoltre le navi da crociera anche nel 2018 hanno scaricato più di 4,2 miliardi di litri di liquami negli oceani.'
 }, {
   d1: 'Quale oggetto è il più inquinante per l\'ambiente?',
-  risp1: ['Chewing Gum', 'Pneumatico', 'Sigarette', 'Cotton Fioc'],
+  risp1: ['Sigarette', 'Pneumatico', 'Chewing Gum', 'Cotton Fioc'],
   c1: 2,
 
   d2: 'Dove si trova l\'inceneritore più grande d\'Europa?',
   risp2: ['Monaco', 'Brescia', 'Stoccolma', 'Le Mans'],
-  c2: 2,
-  desc: 'Greve zi'
+  c2: 1,
+  desc: 'In meno di trent’anni il mondo sarà completamente sommerso dai rifiuti, a meno che non si mettano in campo soluzioni condivise di corretta gestione dei rifiuti, soprattutto nei Paesi a basso reddito che al momento non hanno a disposizione sufficienti risorse per avviare una filiera di riciclo sostenibile. <br><br> 1) Le gomme da masticare hanno un ruolo tristemente importante, perché sono fatte di un polimero del petrolio e di un lubrificante dannoso per l’ambiente. <br> Rimuoverle dai nostri marciapiedi costa 10 volte più del prodotto stesso.<br>Ogni anno però ne vengono vendute oltre 560mila tonnellate ed è il secondo rifiuto più comune.<br><br> 2)L\'inceneritore più grande d\'Europa è quello di Brescia, considerato anche quello più avanzato tecnologicamente dagli Scienziati, ma tuttavia viene criticato per l\'inquinamento che rilascia'
 
 }, {
   d1: 'Quanti animali muoiono ogni anno per i rifiuti di plastica negli oceani?',
   risp1: ['3mln', '1.5mln', '2.5mln', '500.000'],
-  c1: 2,
+  c1: 1,
 
   d2: 'Quali tra questi animali marini è quello più a rischio di esitinzione?',
   risp2: ['Balenottere Azzurre', 'Delfino Vaquita', 'Tartaruga Embricata', 'Tonno Rosso'],
   c2: 2,
-  desc: 'Greve zi'
+  desc: 'L\'oceano questa immensa distesa d\'acqua blu che ricopre parte del nostro pianeta diventa sempre di più la discarica della Terra.<br> Dai sacchetti di plastica ai pesticidi, la maggiore parte dei rifiuti prodotti dall\'uomo finisce in un modo o nell\'altro in mare.<br> Anche le navi e le piattaforme petrolifere fanno la loro parte. Il WWF si adopera per richiamare all\'ordine autorità e aziende affinché siano elaborate e adottate misure volte alla tutela delle acque marine'
 
 }]
